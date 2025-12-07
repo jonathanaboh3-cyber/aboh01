ab02f8880661adc551ba242c2f7d1d1c7b41a479
JONATHAN AHUBI <joahubi@noreply.acad.learn2earn.ng>
2025-12-07 22:28:32 +0000

    Add enigma.go

---
+++ enigma.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	tempA := ***a
+	tempB := *b
+	tempC := *******c
+	tempD := ****d
+
+	*******c = tempA
+	****d = tempC
+	*b = tempD
+	***a = tempB
+}
