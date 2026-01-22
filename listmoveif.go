77e5fde909a7d513c44185187f10518ead544fbb
joahubi <jonathanaboh1@gmail.com>
2026-01-22 10:51:14 +0100

    listmoveif

---
+++ listmoveif.go
@@ -0,0 +1,27 @@
+package piscine
+
+func ListRemoveIf(l *List, data_ref interface{}) {
+	if l == nil || l.Head == nil {
+		return
+	}
+
+	for l.Head != nil && l.Head.Data == data_ref {
+		l.Head = l.Head.Next
+	}
+
+	if l.Head == nil {
+		l.Tail = nil
+		return
+	}
+
+	current := l.Head
+	for current.Next != nil {
+		if current.Next.Data == data_ref {
+			current.Next = current.Next.Next
+		} else {
+			current = current.Next
+		}
+	}
+
+	l.Tail = current
+}
